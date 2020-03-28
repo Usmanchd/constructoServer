3d06e93267e2b556742561306211751c3d4b3205
Usman <usman.aslam0701@gmail.com>
2020-03-28 16:57:03 +0500

    first commit

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const app = express();
 const path = require('path');
 
 app.use(express.static('client/build'));
-console.log('here');
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
-console.log('here');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
