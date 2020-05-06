c4f978f8a7a89d0af99f8915fa2756de102169af
Usmanchd <usman.aslam0701@gmail.com>
2020-05-06 21:55:12 +0500

    first commit

--- client/src/router.js
+++ client/src/router.js
@@ -51,8 +51,6 @@ const routes = [
 class Router extends React.Component {
   render() {
     const { history } = this.props
-    const hide = true
-    if (hide) return <h2>Maintenance Mode!</h2>
     return (
       <ConnectedRouter history={history}>
         <IndexLayout>
